0d21e16563927d895a1b6bd144f2756b975cf86c
Djoudi21 <abdelkrim@gdpr.dev>
2023-07-09 12:20:23 +0200

    feat: commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,28 +7,28 @@ export default function Form() {
     return (
         <form onSubmit={form.handleSubmit} className="row g-3" id="create-employee">
             {/*FIRSTNAME*/}
-            <Input label={'First Name'} type={'text'} id={'first-name'} setInputValue={form.handleSetFirstname} />
+            <Input  required label={'First Name'} type={'text'} id={'first-name'} setInputValue={form.handleSetFirstname} />
 
             {/*LASTNAME*/}
-            <Input label={'Last Name'} type={'text'} id={'last-name'} setInputValue={form.handleSetLastname} />
+            <Input required label={'Last Name'} type={'text'} id={'last-name'} setInputValue={form.handleSetLastname} />
 
 
             {/*DATE OF BIRTH*/}
-            <Input label={'Date of Birth'} type={'date'} id={'date-of-birth'} setInputValue={form.handleSetDateOfBirth} />
+            <Input required label={'Date of Birth'} type={'date'} id={'date-of-birth'} setInputValue={form.handleSetDateOfBirth} />
 
 
             {/*START DATE*/}
-            <Input label={'Start date'} type={'date'} id={'start-date'} setInputValue={form.handleSetStartDate} />
+            <Input required label={'Start date'} type={'date'} id={'start-date'} setInputValue={form.handleSetStartDate} />
 
             {/*ADDRESS*/}
-            <Input label={'Street'} type={'text'} id={'street'} setInputValue={form.handleSetStreet} />
-            <Input label={'City'} type={'text'} id={'city'} setInputValue={form.handleSetCity} />
-            <Select options={form.statesOptions} label={'State'}  name={'states'} id={'states'} setSelectValue={form.handleSetState} />
-            <Input label={'ZipCode'} type={'text'} id={'zip-code'} setInputValue={form.handleSetZipCode} />
+            <Input required label={'Street'} type={'text'} id={'street'} setInputValue={form.handleSetStreet} />
+            <Input required label={'City'} type={'text'} id={'city'} setInputValue={form.handleSetCity} />
+            <Select required options={form.statesOptions} label={'State'}  name={'states'} id={'states'} setSelectValue={form.handleSetState} />
+            <Input required label={'ZipCode'} type={'text'} id={'zip-code'} setInputValue={form.handleSetZipCode} />
 
             {/*DEPARTMENT*/}
-            <Select options={form.departmentsOptions} label={'Departments'} name={'departments'} id={'departments'} setSelectValue={form.handleSetDepartment} />
-            <button className="btn btn-primary" type="submit">Save</button>
+            <Select required options={form.departmentsOptions} label={'Departments'} name={'departments'} id={'departments'} setSelectValue={form.handleSetDepartment} />
+            <button className="btn btn-primary">Save</button>
         </form>
     )
 }
